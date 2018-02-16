07eec9d447b1c97cd2f144eddce2b4282d1b5967
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 03:14:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  !position_taken?(board, index) && board.length > index
+  position_taken?(board, index) && board.length > index
 end
 
 def position_taken?(board, index)
