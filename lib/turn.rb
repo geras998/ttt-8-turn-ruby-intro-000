5dc95c79430ae5efc81f14339db67d319c73570d
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 03:11:18 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,11 @@
 def display_board(board)
+  9.times do |i|
+    print " #{board[i]} "
+    if (i+1) % 3 == 0 and i < 8 then
+      puts ""
+      puts "-" * 11
+    end
+    print "|" if (i+1) % 3 != 0
+  end
+  puts ""
 end
